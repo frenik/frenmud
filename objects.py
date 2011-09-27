45ef6868755f39136d0c59be9c95bb4a9db30ef7
Nick Myers <frenik@gmail.com>
2011-09-27 16:19:29 -0400

    Implemented new file read function for objects. Time for my day job.

--- objects.py
+++ objects.py
@@ -1,22 +1,9 @@
+import files
 import os
 
 class Object:
     def __init__(self, owner, file):
-        settings = {}
-        # open file
-        f = open("objects\\"+file+".obj", "r")
-        # iterate through file
-        while f:
-            line = f.readline()
-            # test for empty line
-            if line=='': break
-            line = line.rstrip('\n')            
-            # comment lines start with '#', discard line and continue
-            if line.find('#')==0: continue            
-            # split line into key:value
-            line = line.split(':')            
-            # create new value to key in dict
-            settings[line[0]] = line[1]
+        settings = files.loadFromFile('objects\\%s.obj'%file)
             
         # iterate through dict
         for k in settings.keys():
