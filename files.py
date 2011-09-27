d9ffc32201c7b30119375fa2ac39ed6c2c29559c
Nick Myers <frenik@gmail.com>
2011-09-27 16:10:24 -0400

    Added file utility function loadFromFile(filename)

---
+++ files.py
@@ -0,0 +1,32 @@
+#   files.py contains functions used for utility, involving (obviously)
+# file manipulation
+
+def loadFromFile(filename):
+    ''' Get a file's contents and return a dictionary containing values.
+       :param filename: The filename to be loaded, including full path.
+    '''
+    try:
+        f = open(filename,'r')
+    except IOError:
+        # error opening file
+        return None
+    
+    # empty dictionary to store settings from file
+    settings = {}
+    
+    # loop through file
+    while f:
+        line = f.readline()
+        # test for empty line, end read if found
+        if line=="": break 
+        # strip endline
+        line = line.rstrip('\n')
+        # test for comment line, discard
+        if line.find('#') == 0: # comment lines start with # on first space
+        # split line by colon character
+        line = line.split(':')
+        # store everything up to first colon as key, everything after as value
+        settings[line[0]] = line[1]
+    
+    # return the dictionary
+    return settings
